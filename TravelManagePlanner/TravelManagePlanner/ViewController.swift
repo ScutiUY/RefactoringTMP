58090327f99f6987cac6722b12b7c0b9191b7ce5
UyeonS <dusdl14@gmail.com>
2021-12-26 16:45:19 +0900

    TEST

---
+++ TravelManagePlanner/TravelManagePlanner/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TravelManagePlanner
+//
+//  Created by UY on 2021/12/26.
+//
+
+import UIKit
+import SnapKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

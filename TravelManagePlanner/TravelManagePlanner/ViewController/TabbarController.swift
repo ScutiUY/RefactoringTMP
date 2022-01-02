8dfd8c22082407737a8496275b55c5d1ceea5191
UyeonS <dusdl14@gmail.com>
2022-01-02 19:46:55 +0900

    TabbarController
    탭바 컨트롤러 추가(이미지, 타이틀)

--- TravelManagePlanner/TravelManagePlanner/ViewController/TabbarController.swift
+++ TravelManagePlanner/TravelManagePlanner/ViewController/TabbarController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class TabbarController: UITabBarController {
-
+    
+    /// Tabbar Controller
+    /// 해당 Tabbar Controller에서 이동 할 수 있는 VC를 TabbarItem으로 추가해 주면 됩니다.
+    /// 추가해 놓은 VC는 샘플이므로 추가할 VC로 교체 하면 됩니다.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // VC 선언 부분
+        let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SampleViewController")
+        
+        let sampleVC1 = SampleViewController() // 여행 리스트 VC
+        let sampleVC2 = SampleViewController() // 커뮤니티 VC
+        let sampleVC3 = SampleViewController() // 마이페이지 VC
+        
+        // 홈 탭
+        // Tab의 VC 목록에 해당 VC를 넣을 때 Navigation Controller에 넣어서 추가하면 네비게이션 컨트롤러를 자동으로 상속 합니다
+        let homeTab = UINavigationController(rootViewController: homeVC)
+        homeTab.tabBarItem.image = UIImage(named: "Home2")!.resizedImage(targetSize: CGSize(width: 25, height: 25))
+        homeTab.title = "home"
+        // 여행 리스트 탭
+        let planTab = UINavigationController(rootViewController: sampleVC1)
+        planTab.tabBarItem.image = UIImage(named: "List3")!.resizedImage(targetSize: CGSize(width: 25, height: 25))
+        planTab.title = "journey"
+        
+        // 커뮤니티 탭
+        let commuTab = UINavigationController(rootViewController: sampleVC2)
+        commuTab.tabBarItem.image = UIImage(named: "Community2")!.resizedImage(targetSize: CGSize(width: 25, height: 25))
+        commuTab.title = "community"
+        
+        // 마이 페이지 탭
+        let myPageTab = UINavigationController(rootViewController: sampleVC3)
+        myPageTab.tabBarItem.image = UIImage(named: "MyPage")!.resizedImage(targetSize: CGSize(width: 25, height: 25))
+        myPageTab.title = "myPage"
+        
+        self.viewControllers = [homeTab, planTab, commuTab, myPageTab]
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

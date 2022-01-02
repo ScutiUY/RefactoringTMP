3154d8049e691e2962921fd7557cea595002a851
UyeonS <dusdl14@gmail.com>
2022-01-03 01:51:03 +0900

    Add Globalconstrants
    공통 상수를 다룰 GlobalConstrants 추가

--- TravelManagePlanner/TravelManagePlanner/MISC/GlobalConstrants.swift
+++ TravelManagePlanner/TravelManagePlanner/MISC/GlobalConstrants.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+enum GlobalConstants {
+    
+    enum Font {
+        enum Size {
+            static var mainFontSize: CGFloat = 15
+            static var descFontSize: CGFloat = 10
+        }
+    }
+    
+    enum Color {
+        enum Text {
+            static var buttonTextColor: UIColor = .white
+            static var invalidateTextColor: UIColor = .red
+        }
+        enum Background {
+            static var themeColor: UIColor = UIColor(red: 131/255, green: 247/255, blue: 178/255, alpha: 1)
+            static var loginButtonbackgroundColor: UIColor = .blue
+        }
+        
+    }
+    
+}

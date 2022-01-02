0416c32b384622efb4e097dd4341747e77b6a37b
UyeonS <dusdl14@gmail.com>
2022-01-03 03:48:06 +0900

    Add Travel Data Model
    DetailView를 위한 Data Model

--- TravelManagePlanner/TravelManagePlanner/Model/TravelData.swift
+++ TravelManagePlanner/TravelManagePlanner/Model/TravelData.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+struct JourneyData: Codable {
+    static let shared = JourneyData(title: "", desc: "", images: [])
+    let title: String
+    let desc: String
+    var images = [JourneyImage]()
+    
+    init(title: String, desc: String, images: [JourneyImage]) {
+        self.title = title
+        self.desc = desc
+        self.images = images
+    }
+}
+
+struct JourneyImage: Codable {
+    static var empty = JourneyImage(title: "", link: "", height: "", width: "")
+    let title: String
+    let link: String
+    let height: String
+    let width: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case link
+        case height = "sizeheight"
+        case width = "sizewidth"
+    }
+}

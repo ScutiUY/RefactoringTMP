f92ad146a20a75e6621fcf4339833172aafa5791
UyeonS <dusdl14@gmail.com>
2022-01-02 19:43:47 +0900

    Add resizedImage function by UIImage extension
    이미지 크기 변경을 위한 함수를 extension으로 추가

--- TravelManagePlanner/TravelManagePlanner/MISC/UIImage+extension.swift
+++ TravelManagePlanner/TravelManagePlanner/MISC/UIImage+extension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImage {
+
+    func resizedImage(targetSize: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+}

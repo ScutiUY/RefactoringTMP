e8d5db40674009165cf262cdad4bc225edf98ea4
UyeonS <dusdl14@gmail.com>
2022-01-02 19:45:55 +0900

    Add VC for sample Tabbar
    Tabbar 가이드를 위한 샘플 VC 추가

--- TravelManagePlanner/TravelManagePlanner/ViewController/SampleViewController.swift
+++ TravelManagePlanner/TravelManagePlanner/ViewController/SampleViewController.swift
@@ -16,7 +16,7 @@ class SampleViewController: UIViewController {
         super.viewDidLoad()
         view.addSubview(abutton)
         view.backgroundColor = .white
-        self.title = "home"
+        
         abutton.snp.makeConstraints { make in
             make.center.equalToSuperview()
             make.width.equalTo(100)
@@ -27,7 +27,7 @@ class SampleViewController: UIViewController {
         
     }
     @objc func buttonAction() {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondSampleViewController") as! SecondSampleViewController
         navigationController?.pushViewController(vc, animated: true)
     }
 

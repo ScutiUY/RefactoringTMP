7cd096d7b9826cf28488014f4c0e1080c79aa8d6
UyeonS <dusdl14@gmail.com>
2022-01-10 08:41:21 +0900

    Update Travel Model Data

--- TravelManagePlanner/TravelManagePlanner/Model/Travel.swift
+++ TravelManagePlanner/TravelManagePlanner/Model/Travel.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 //struct Travel: Codable {
-//    
+//
 //    static var shared = Travel(type: .accommodation, name: "", desc: "", image: "")
-//    
+//
 //    let type: ContentType
 //    let name: String
 //    let desc: String
 //    let image: String
-//    
+//
 //    init(type: ContentType, name: String, desc: String, image: String) {
 //        self.type = type
 //        self.name = name

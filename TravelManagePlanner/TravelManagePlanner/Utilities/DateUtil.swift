41cc6d81bb5be124058666c56ae79970932d9ec8
UyeonS <dusdl14@gmail.com>
2022-01-10 08:38:34 +0900

    Update DateUitlities

--- TravelManagePlanner/TravelManagePlanner/Utilities/DateUtil.swift
+++ TravelManagePlanner/TravelManagePlanner/Utilities/DateUtil.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class DateUtil {
+    static func parseDate(_ dateString: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+
+        return formatter.date(from: dateString) ?? Date()
+    }
+
+    static func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+
+        return formatter.string(from: date)
+    }
+
+    static func dueString(start: Date, end: Date) -> String {
+        return "\(formatDate(start)) ~ \(formatDate(end))"
+    }
+}

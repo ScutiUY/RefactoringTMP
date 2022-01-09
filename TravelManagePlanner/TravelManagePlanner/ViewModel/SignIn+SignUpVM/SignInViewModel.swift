f9a2638ebfd4b7f92a0fa1ca302c8d32551c1789
UyeonS <dusdl14@gmail.com>
2022-01-10 06:09:59 +0900

    Grouping Sign VM

--- TravelManagePlanner/TravelManagePlanner/ViewModel/SignIn+SignUpVM/SignInViewModel.swift
+++ TravelManagePlanner/TravelManagePlanner/ViewModel/SignIn+SignUpVM/SignInViewModel.swift
@@ -6,26 +6,65 @@
 //
 
 import Foundation
+import UIKit
+
+
+enum ValidationResult {
+    case success
+    case invalidEmail
+    case invalidPwd
+}
 
 class SignInViewModel {
     
     //private let signInData: UserData
-    
-    private var userInfo = UserData(userEmail: "", userPw: "", name: "") {
+    private var api = APIRequest()
+    private var userInfo = UserData(email: "", pw: "", name: "") {
         didSet {
-            username = userInfo.name!
             email = userInfo.userEmail!
-            pwd = userInfo.userPw!
+            password = userInfo.userPw!
             
         }
     }
     
-    private var username = ""
+    var userInfoInputErrorMessage: Observable<String> = Observable("")
+    var loadingStarted: Observable<Bool> = Observable(false)
+    var loadingEnded: Observable = Observable("")
+    
     private var email = ""
-    private var pwd = ""
+    private var password = ""
     
+    func updateUserEmail(email: String) {
+        self.email = email
+    }
+    func updateUserPwd(password: String) {
+        self.password = password
+    }
+    func inputUserInfo(textField: UITextField) {
+        
+        print(textField)
+    }
     
-    
+    func validateUserInformation() -> ValidationResult {
+        let regex = try? NSRegularExpression(pattern: "[A-Z0-9a-z._&+-]+@[A-Z0-9a-z.-]+[.]+[A-Za-z]{2,20}", options: NSRegularExpression.Options.caseInsensitive)
+        let textToNS = email as NSString
+        let emailCheck = regex?.matches(in: email, options: [], range: NSRange(location: 0, length: textToNS.length)).map{textToNS.substring(with: $0.range)}
+        
+        if emailCheck!.isEmpty || email == "" {
+            userInfoInputErrorMessage.value = "이메일을 확인해 주세요"
+            return .invalidEmail
+        } else if password == "" || password.count < 6 {
+            userInfoInputErrorMessage.value = "비밀번호를 확인 해주세요"
+            return .invalidPwd
+        } else {
+            return .success
+        }
+    }
+    func login() {
+        LoginData.shared.email = email
+        LoginData.shared.password = password
+        api.login(loginData: LoginData.shared)
+    }
 }
 
 extension SignInViewModel {
--- TravelManagePlanner/TravelManagePlanner/ViewModel/SignIn+SignUpVM/SignUpViewModel.swift
+++ TravelManagePlanner/TravelManagePlanner/ViewModel/SignIn+SignUpVM/SignUpViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class SignUpViewModel {
+    private var api = APIRequest()
+    
+    var userInfoInputErrorMessage: Observable<String> = Observable("")
+    
+    private var email = ""
+    private var password = ""
+    private var name = ""
+    func updateUserEmail(email: String) {
+        self.email = email
+    }
+    func updateUserPwd(password: String) {
+        self.password = password
+    }
+    func updateUserName(name: String) {
+        self.name = name
+    }
+    
+}

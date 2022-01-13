ad49305d77c92e7efd8d840c6eac52b23da2ade6
UyeonS <dusdl14@gmail.com>
2022-01-13 11:58:51 +0900

    Add Struct Model
    Create JourneyListData for API Decoding

--- TravelManagePlanner/TravelManagePlanner/Model/APIResponseData.swift
+++ TravelManagePlanner/TravelManagePlanner/Model/APIResponseData.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+struct JourneyListResData {
+    static var shared = JourneyListResData(resCode: "", resMsg: "", data: "")
+    var resCode: String
+    var resMsg: String
+    var data: String
+    init(resCode: String, resMsg: String, data: String) {
+        self.resCode = resCode
+        self.resMsg = resMsg
+        self.data = data
+    }
+}

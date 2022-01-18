f37292de66d7496d92e5d422086f3dd15a64ed0a
UyeonS <dusdl14@gmail.com>
2022-01-18 10:17:10 +0900

    Update JourneyList MV
    Add JourneyList MV

--- TravelManagePlanner/TravelManagePlanner/ViewModel/JourneyListViewModel.swift
+++ TravelManagePlanner/TravelManagePlanner/ViewModel/JourneyListViewModel.swift
@@ -9,6 +9,31 @@ import Foundation
 
 class JourneyListViewModel {
     
+    private var api = APIRequest()
     
+    var journeyList: JourneyList = JourneyList.shared {
+        didSet {
+            
+        }
+    }
+    
+    
+    var loadingStarted: Observable = Observable("")
+    var loadingEnded: Observable = Observable("")
+    var journeyListUpdated: Observable = Observable("")
+    
+    func getPlanDataFromAPI() {
+        api.getJourneyList { result in
+            switch result {
+            case .success(let str):
+                self.journeyList
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+    }
+    func count() -> Int {
+        return journeyList.journeys.count
+    }
     
 }

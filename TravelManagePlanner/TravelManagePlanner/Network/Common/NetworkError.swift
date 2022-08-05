6fea2f7b298bd933ccd2ed0a28749d0ae9d93d8e
신의연 <dusdl14@gmail.com>
2022-08-05 19:44:12 +0900

    [refact] 네트워크 에러처리를 위한 enum 파일

--- TravelManagePlanner/TravelManagePlanner/Network/Common/NetworkError.swift
+++ TravelManagePlanner/TravelManagePlanner/Network/Common/NetworkError.swift
@@ -8,12 +8,7 @@
 import Foundation
 
 enum APIError: Int, Error {
-    case unknown = -1
-    case jsonError = -2
-    case invalidArgument = -3
-    case badRequest = 400
-    case notFound = 404
-    case internalServerError = 500
+    
     case omittedParams = 9992
     case ommittedHeader = 4444
     case notFoundInDB = 3001
@@ -21,16 +16,7 @@ enum APIError: Int, Error {
     
     var errorDescription: String? {
         switch self {
-        case .unknown: return "알수 없는 에러입니다."
-        case .badRequest: return "request 오류 입니다."
-        case .jsonError:
-            return "json 에러"
-        case .invalidArgument:
-            return "유효하지 않은 아규먼트"
-        case .notFound:
-            return "404 notFound"
-        case .internalServerError:
-            return "서버 오류"
+            
         case .omittedParams:
             return "파라미터 누락"
         case .ommittedHeader:
@@ -39,6 +25,34 @@ enum APIError: Int, Error {
             return "결과를 DB에서 찾을수 없습니다."
         case .invalidPw:
             return "비밀번호 오류"
+        }
+    }
+}
+
+enum NetworkError: Error {
+    
+    case invalidURL
+    case requestFail(Error)
+    case invalidResponse
+    case failedResponse(statusCode: Int)
+    case emptyData
+    case decodeError
+    
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "유효하지 않은 URL입니다."
+        case .requestFail:
+            return "HTTP 요청에 실패했습니다."
+        case .invalidResponse:
+            return "유효하지 않은 응답입니다."
+        case .failedResponse(let statusCode):
+            return "(\(statusCode)) 실패한 상태코드"
+        case .emptyData:
+            return "데이터가 없습니다."
+        case .decodeError:
+            return "디코딩 실패"
         }
     }
 }

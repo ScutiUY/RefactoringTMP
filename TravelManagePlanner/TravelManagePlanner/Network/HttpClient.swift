320269bb5fd335dd6d7abbe2ed54ae9f5ee9c784
신의연 <dusdl14@gmail.com>
2022-08-07 20:01:09 +0900

    [refact] AF request 통신 레이어 리팩토링

--- TravelManagePlanner/TravelManagePlanner/Network/HttpClient.swift
+++ TravelManagePlanner/TravelManagePlanner/Network/HttpClient.swift
@@ -11,25 +11,32 @@ import Alamofire
 struct HttpClient {
     
     func request(endPoint: TargetType, completion: @escaping (Result<Data, Error>) -> Void) {
-        
-        AF.request(endPoint).responseJSON { responseData in
-            if let error = responseData.error {
-                completion(.failure(NetworkError.requestFail(error)))
+        do {
+            let request = try endPoint.asURLRequest()
+            
+            AF.request(request).responseJSON { responseData in
+                if let error = responseData.error {
+                    completion(.failure(NetworkError.requestFail(error)))
+                }
+                guard let response = responseData.response else {
+                    completion(.failure(NetworkError.invalidResponse))
+                    return
+                }
+                guard 200..<299 ~= response.statusCode else {
+                    completion(.failure(NetworkError.failedResponse(statusCode: response.statusCode)))
+                    return
+                }
+                guard let data = responseData.data else {
+                    completion(.failure(NetworkError.emptyData))
+                    return
+                }
+                completion(.success(data))
             }
-            guard let response = responseData.response else {
-                completion(.failure(NetworkError.invalidResponse))
-                return
-            }
-            guard 200..<299 ~= response.statusCode else {
-                completion(.failure(NetworkError.failedResponse(statusCode: response.statusCode)))
-                return
-            }
-            guard let data = responseData.data else {
-                completion(.failure(NetworkError.emptyData))
-                return
-            }
-            completion(.success(data))
+        } catch let requestError {
+            completion(.failure(NetworkError.requestFail(requestError)))
         }
+        
+        
     }
     
 }
